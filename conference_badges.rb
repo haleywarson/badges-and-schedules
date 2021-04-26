da9814fe81ec5ac2c5085ff6fa9154d5b13f19c4
Haley Warson <haleywarson@gmail.com>
2021-04-26 17:08:04 -0600

    assignment complete

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
 # Write your code here.
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    badge_messages = []
+    names.each do |name|
+        badge_messages << badge_maker(name)
+    end
+    badge_messages
+end    
+
+def assign_rooms(speakers)
+    room_assignments = []
+    speakers.each_with_index do |speaker, i|
+        room_assignments << "Hello, #{speaker}! You'll be assigned to room #{i + 1}!"
+    end
+
+    room_assignments
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each do |value|
+        puts value
+    end
+    assign_rooms(attendees).each do |value|
+        puts value
+     end
+end
